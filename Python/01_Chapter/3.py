33e9b08ccce50990e09f428687844959d856068e
Manvanthmanu <manvanthmn1234@gmail.com>
2022-10-28 19:06:04 +0530

    28-10-2022

---
+++ Python/01_Chapter/3.py
@@ -0,0 +1,11 @@
+# data types ( primitive data types )
+
+# alphabets ( char /  string ) 
+    # always write strings inside quotes
+
+# numeric (int , float , Ascii , bits ) 
+    # never put numbers inside quotes 
+
+# boolean 
+    # true 1 / false 0
+    # do not put boolean inside a quotes 
---
+++ Python/01_Chapter/4.py
@@ -0,0 +1,17 @@
+#  Data types ( non primitive data types ) objects 
+
+# data structures stores set of primitive data types 
+
+# objects
+
+# list = ['manu' , 35 , True ] 
+# tupple
+# dictionary
+# set
+# arrays
+# graphs
+# qeue 
+
+y =[3,2,4,5,3,3]
+
+x =[2,3,5,9,y]
