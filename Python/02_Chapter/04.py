95e96ffe028c014bfb7d142e9108ee2826db18fd
Manvanthmanu <manvanthmn1234@gmail.com>
2022-11-01 19:38:24 +0530

    1/11/22

---
+++ Python/02_Chapter/04.py
@@ -0,0 +1,20 @@
+#  basic calculator 
+
+# =============== order of operations ===================== 
+
+# pemdas rule 
+# parentheses, exponents, multiplication, division, addition, subtraction
+
+# bedmas 
+# bracket , exponents ,  division ,multiplication ,addition and subtraction 
+
+# paranthrses () ,[] ,{}
+# exponents **
+# multiplication
+# division
+# addition
+# subtraction 
+
+# a = (5*6/8**4+4-4)
+
+# print(a)
---
+++ Python/02_Chapter/excercise1.py
@@ -0,0 +1,50 @@
+
+# 1 - calculate simple intrest  (3)
+# step 1- start
+# step 2 - input(p,t,r)
+# step 3- si=(p*t*r/100)
+# step 4- output(si)
+# step 5 - end
+
+# 2 - convert cm to m  (2.5)
+# step 1- start 
+# step 2- input(cm)
+# step 3- conversion=cm/100
+# step 4 - conversion
+# step 5- end 
+
+# 3 calculate the area of a sphere (3)
+# step 1 - start
+# step 2 - input(r)
+# step 3- areaofasphere=(4*22/7*r*r)
+# step 4- areaofasphere
+# step 5 - end
+
+# 4 calculate perimeter of rectangle (3)
+# step 1 - start
+# step 2 - input(a,b)
+# step 3 - perimeterofarectangle= 2*(a+b)
+# step 4 - perimeterofrectangle
+# step6- end
+
+# 5 calculate compound intrest  (4)
+# step 1 - start
+# step 2 - input(p,r,n)
+# step 3 - ci=p*(1+r/100)n-p
+# step 4 - ci
+# step 5 - end
+
+
+
+# 6  calculate compond interest and give output as a and ci   
+# step 1 - start
+# step 2 - input(P ,r,n ,t)
+# step 3 - A = P(1 + r/n)^(nt)
+# step 4 - ci = A - P
+# step 5 - ci , A
+# step 6 - end
+
+
+# total marks - 15.5/20
+
+# 3/20
