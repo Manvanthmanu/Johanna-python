957b3c3d5d3ef33841317b001bab7e1ee8c08e80
Manvanthmanu <manvanthmn1234@gmail.com>
2022-11-11 20:22:03 +0530

    11/11/22

---
+++ Python/03_Chapter/04.py
@@ -0,0 +1,91 @@
+#  Objects - list / tupple / set / dictionary
+
+# List - list methods 
+# list is a collection of items 3
+# list is mutable / it can be altered , changed
+# list can store any type of data 
+# you can add a list inside a list 
+# some list methods change the original list
+
+# lst1 = ['pineapple' , 'mango' , 'banana' , 'apple' , 'cheery'] # - each element in a list is called a item
+# lst2 =['carrot' , 'beans' , 'onions' , 'tomoto' , 'potato']
+# lst3 = ['vanila' , 'butterscotch','chocolate','pista' ,'black current']
+# lst4 = [101,102,103,104,105]
+# lst5 = [101.4,102.5,103.5,104.9,105.3]
+# lst6 = [True , False]
+# lst7 =['india' , 5 ,1.5 , True]
+
+# MainList = [lst1 , lst2 , lst3]
+# print(MainList)
+
+
+
+# list methods 
+lst4 = [101,102,103,104,105,130,190,65,10,20,90]
+
+sum1 = sum(lst4)
+print(sum1)
+
+# lst4[3] =1000
+
+
+# var1 = 'manvanth'
+# lst4.append(var1)
+# lst4.sort()
+# lst4.reverse()
+# lst4.remove(101)
+# lst4.pop()
+# lst4.insert(2 ,30)
+# lst5.count(20)
+
+# newlist =lst4.copy()
+# newlist.pop()
+
+
+print(lst4)
+
+
+
+
+
+# tupple 
+# lst1 = ['jo','di' , 'keer' ,'snh','neha']
+# tup1 = ( 18,19,18,17,16 )
+
+
+
+# set - set methods
+# set1 = {10,20,30,40,50}
+
+# add = sum(set1)
+# print(add/len(set1))
+
+
+
+# dict - set methods 
+
+dict1 = {
+    'sophisticated':'having, revealing, or involving a great deal of worldly experience and knowledge of fashion and culture.' ,
+    'Johana':'she is a good girl',
+    'rat':'it is a annoying creature',
+    'bottle':'bottle stores any liquid'
+}
+
+
+johana = {
+    'gender':'female',
+    'age':19,
+    'read books':True
+}
+rollnum = {
+    'johana':johana,
+    'manvanth':39,
+    'keerthana':33,
+    'allen':7
+}
+
+# add value
+# delete value
+# update
+
+
